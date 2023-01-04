cc3f844ec399e29da953dd57c65c2a29da8ea83f
Ngonidzashe Mangudya <imngonii@gmail.com>
2023-01-04 23:10:01 +0200

    feat: whatsapp session model

--- bot/models.py
+++ bot/models.py
@@ -1,3 +1,60 @@
 from django.db import models
 
-# Create your models here.
+from mboma.model import SoftDeleteModel
+
+
+class WhatsappSession(SoftDeleteModel):
+    phone_number = models.CharField(max_length=255, blank=True, null=True)
+    stage = models.CharField(max_length=255, blank=True, null=True)
+    position = models.CharField(max_length=255, blank=True, null=True)
+    payload = models.JSONField(blank=True, null=True, default=dict)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.phone_number}"
+
+    class Meta:
+        db_table = "whatsapp_sessions"
+        verbose_name = "Whatsapp Session"
+        verbose_name_plural = "Whatsapp Sessions"
+        table_prefix = "ses"
+
+    @classmethod
+    def create_whatsapp_session_or_get_whatsapp_session(
+        cls, phone_number, stage, position, payload=None
+    ):
+        whatsapp_session = cls.objects.filter(phone_number=phone_number).first()
+        if whatsapp_session:
+            whatsapp_session.stage = stage
+            whatsapp_session.position = position
+            whatsapp_session.payload = payload
+            whatsapp_session.save()
+            return whatsapp_session
+        else:
+            whatsapp_session = cls.objects.create(
+                phone_number=phone_number,
+                stage=stage,
+                position=position,
+                payload=payload,
+            )
+            return whatsapp_session
+
+    @classmethod
+    def update_whatsapp_session(cls, phone_number, stage, position, payload):
+        whatsapp_session = cls.objects.filter(phone_number=phone_number).update(
+            stage=stage, position=position, payload=payload
+        )
+        return whatsapp_session
+
+    @classmethod
+    def get_whatsapp_session(cls, phone_number):
+        whatsapp_session = cls.objects.filter(phone_number=phone_number).first()
+        return whatsapp_session
+
+    def reset_to_menu(self):
+        self.stage = "menu"
+        self.position = "menu"
+        self.payload = {}
+        self.save()
+        return self
