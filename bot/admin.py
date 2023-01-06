806a55700f897cc09c54fd75689be8c3d210505d
Ngonidzashe Mangudya <imngonii@gmail.com>
2023-01-06 17:26:22 +0200

    fix: register whatsapp session model

--- bot/admin.py
+++ bot/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
+from bot.models import WhatsappSession
+
+
+@admin.register(WhatsappSession)
+class UniversalAdmin(admin.ModelAdmin):
+    def get_list_display(self, request):
+        return [field.name for field in self.model._meta.concrete_fields]
+
+
+from django.contrib import admin
+
 # Register your models here.
