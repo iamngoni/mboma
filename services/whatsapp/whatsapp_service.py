b8265ab19922986f3bc698735f4009da3a37eada
Ngonidzashe Mangudya <imngonii@gmail.com>
2023-02-12 13:48:28 +0200

    revert

--- services/whatsapp/whatsapp_service.py
+++ services/whatsapp/whatsapp_service.py
@@ -354,7 +354,7 @@ class WhatsappService:
                 text="Choose Product To Retrieve More Information",
                 phone_number=self.formatted_message.get("from_phone_number"),
                 section_title="Products",
-                catalog_id="3469193853364820",
+                catalog_id=config("CATALOG_ID"),
                 products=products,
             )
 
