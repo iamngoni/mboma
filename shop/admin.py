75f30fd1df062035c5f60990fb978aac2f0d6e3d
Ngonidzashe Mangudya <imngonii@gmail.com>
2023-02-13 10:15:19 +0200

    feat: register order model

--- shop/admin.py
+++ shop/admin.py
@@ -1,9 +1,16 @@
 from django.contrib import admin
 
-from shop.models import ApiRequest, ProductCategory, ProductInventory, Product, Discount
+from shop.models import (
+    ApiRequest,
+    ProductCategory,
+    ProductInventory,
+    Product,
+    Discount,
+    Order,
+)
 
 
-@admin.register(ApiRequest, ProductCategory, ProductInventory, Product, Discount)
+@admin.register(ApiRequest, ProductCategory, ProductInventory, Product, Discount, Order)
 class UniversalAdmin(admin.ModelAdmin):
     def get_list_display(self, request):
         return [field.name for field in self.model._meta.concrete_fields]
